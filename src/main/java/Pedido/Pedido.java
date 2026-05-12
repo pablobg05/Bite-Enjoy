8470675a09ef2fdfc21727af7bb60b51c125dd4c
Paolo <pablorodrigonintendo@gmail.com>
2026-05-12 12:48:26 -0600

    Cambie unas cosas en el POMX para el ejecutable

--- src/main/java/Pedido/Pedido.java
+++ src/main/java/Pedido/Pedido.java
@@ -61,7 +61,7 @@ public class Pedido extends javax.swing.JFrame {
         jPanel2 = new javax.swing.JPanel();
         jLabel3 = new javax.swing.JLabel();
 
-        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
         getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
         jPanel1.setBackground(new java.awt.Color(255, 204, 204));
--- src/main/java/Pedido/RESTAURANTE.java
+++
@@ -1,9 +0,0 @@
-
-package Pedido;
-
-public class RESTAURANTE {
-
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
-}
